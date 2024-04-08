27875fec74406509ac33bfb7f8934ba66dd0adff
Canberk Bekiroğlu <boom.canberk@gmail.com>
2024-04-08 11:56:43 +0300

    fix

--- cmd/chatdev/main.go
+++ cmd/chatdev/main.go
@@ -5,11 +5,14 @@ import (
 	"chatdev-with-go/internal/utils"
 )
 
+var (
+	Prompt string
+)
+
 func main() {
 
 	utils.InitLog()
 	utils.Logger.Info().Msg("Starting ChatDev with Go")
 	config.NewConfig()
 	utils.Logger.Info().Msg("ChatDev with Go started successfully")
-
 }
